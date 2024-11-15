5429f5639ebc13c3fa6eb3c085d2a832c8c7fb8b
hakouguelfen <hakoudev@gmail.com>
2024-11-15 17:49:29 +0100

    Fix: write a more smaller transposition cipher

--- encryptor/ciphers/transposition.py
+++ encryptor/ciphers/transposition.py
@@ -1,31 +1,21 @@
 from encryptor.config.common import CMD
-import math
 
 
 def cipher(text: str, key: int, cmd: CMD) -> str:
-    result = ""
-
-    rows = math.ceil(len(text) / key)
-    letters = [""] * len(text)
+    result = [""] * len(text)
     letter_idx = 0
 
-    for k in range(key):
-        for r in range(rows):
-            index = k + (r * key)
+    for col in range(key):
+        pointer = col
 
+        while pointer < len(text):
             match cmd:
                 case CMD.ENCRYPT:
-                    if index < len(text):
-                        result += text[index]
-
+                    result[col] += text[pointer]
                 case CMD.DECRYPT:
-                    if index < len(text):
-                        letters[index] = text[letter_idx]
-                        letter_idx += 1
+                    result[pointer] = text[letter_idx]
+                    letter_idx += 1
 
-    match cmd:
-        case CMD.ENCRYPT:
-            return result
+            pointer += key
 
-        case CMD.DECRYPT:
-            return "".join(letters)
+    return "".join(result)
